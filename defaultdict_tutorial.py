629aeccec057cad7bd286a08d59f386702a85f62
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-05-31 00:29:10 -0500

    Adding solution for "Defaultdict tutorial"

---
+++ defaultdict_tutorial.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/defaultdict-tutorial/problem
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import defaultdict
+d = defaultdict(list)
+m, n = [int(x) for x in input().split(' ')]
+for i in range(m):
+    d[input()].append(i + 1)
+
+for i in range(n):
+    [print(x, end=' ') for x in d[input()]]
+    print('')
